94443ee7acfe30b4d8e0a2c5487a29f73673900e
houssemyahia <houssem.eddine.yh@gmail.com>
2020-09-18 15:04:51 +0100

    Transfer service

---
+++ src/app/_services/transfer.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransferService {
+
+  constructor(
+    private router:Router,
+  ) { }
+
+  private messageSource = new BehaviorSubject(1);
+  currentMessage = this.messageSource.asObservable();
+  
+  private data;
+
+  changeMessage(message: number) {
+    this.messageSource.next(message)
+    console.log(message);
+
+
+  }
+
+  setData(data){
+    this.data = data;
+    console.log(data);
+  }
+
+  getData(){
+    let temp = this.data;
+    this.clearData();
+    return temp;
+  }
+
+  clearData(){
+    this.data = undefined;
+  }}
